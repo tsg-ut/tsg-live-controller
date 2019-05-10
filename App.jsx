ea45211c5f5907b07667f4b23c64a6db876c8d56
Koki Takahashi <hakatasiloving@gmail.com>
2019-05-11 08:29:21 +0900

    Setup initial codes

---
+++ App.jsx
@@ -0,0 +1,26 @@
+const React = require('react');
+
+require('@babel/polyfill');
+require('core-js/stage/4');
+require('core-js/stage/3');
+require('core-js/stage/2');
+require('core-js/stage/1');
+
+module.exports = class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		};
+	}
+
+	render() {
+		return (
+			<svg
+				width="100%"
+				height="100%"
+				viewBox="0 0 100 200"
+			>
+			</svg>
+		);
+	}
+};
