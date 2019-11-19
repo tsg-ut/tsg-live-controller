dbbf1a97e80c2279f9c886883054122e24d46150
Koki Takahashi <hakatasiloving@gmail.com>
2019-11-20 08:53:48 +0900

    Up-to-date live-config

--- live-configs.js
+++ live-configs.js
@@ -29,4 +29,84 @@ module.exports = {
 		],
 		timer: 75,
 	},
+	marathon: {
+		staffs: [
+			{
+				role: '解説',
+				members: ['ナン', 'kuromunori'],
+				color: 'green',
+			},
+			{
+				role: '駒場チーム',
+				members: ['JP3BGY'],
+				color: 'blue',
+			},
+			{
+				role: '本郷チーム',
+				members: ['fiord'],
+				color: 'red',
+			},
+		],
+		timer: 75,
+	},
+	game2: {
+		staffs: [
+			{
+				role: '解説',
+				members: ['taiyoslime', 'ナン'],
+				color: 'green',
+			},
+			{
+				role: 'プレイヤー',
+				members: ['うら'],
+				color: 'blue',
+			},
+		],
+		timer: 90,
+	},
+	hacking: {
+		staffs: [
+			{
+				role: 'パーソナリティ',
+				members: ['hakatashi', 'JP3BGY'],
+				color: 'green',
+			},
+		],
+		timer: null,
+	},
+	game3: {
+		staffs: [
+			{
+				role: '解説',
+				members: ['moratorium08', 'satos'],
+				color: 'green',
+			},
+			{
+				role: 'プレイヤー',
+				members: ['hakatashi'],
+				color: 'red',
+			},
+		],
+		timer: 90,
+	},
+	ctf: {
+		staffs: [
+			{
+				role: '解説',
+				members: ['hakatashi', 'bitmath'],
+				color: 'green',
+			},
+			{
+				role: '駒場チーム',
+				members: ['ナン', 'JP3BGY', 'taiyoslime'],
+				color: 'blue',
+			},
+			{
+				role: '本郷チーム',
+				members: ['moratorium08', 'fiord', 'satos'],
+				color: 'red',
+			},
+		],
+		timer: 75,
+	},
 };
