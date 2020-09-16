c651ec983e06b0b52d39a1d0bc911f3c2616c254
hideo54 <hideo54@hideo54.com>
2020-09-17 04:13:22 +0900

    Fix a typo (カス…………

--- slack.js
+++ slack.js
@@ -13,7 +13,7 @@ module.exports = async (type, text, url) => {
         username = 'TSG LIVE! 公式サイト 匿名コメント';
         icon_emoji = ':dare:';
     } else if (type === 'twitter') {
-        uesrname = 'Twitter #tsg_live ツイート';
+        username = 'Twitter #tsg_live ツイート';
         icon_emoji = ':twitter:';
     } else if (type === 'youtube') {
         username = 'YouTube Live! コメント';
